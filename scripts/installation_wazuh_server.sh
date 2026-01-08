120ef764f8021e5f3201ce2a141bca452c0c94e9
Youssef-YN <nazihyoussef05@gmail.com>
2026-01-08 10:05:00 +0100

    added scripts

---
+++ scripts/installation_wazuh_server.sh
@@ -0,0 +1,3 @@
+sudo apt update && sudo apt -y upgrade
+curl -sO https://packages.wazuh.com/4.7/wazuh-install.sh
+sudo bash wazuh-install.sh -a
---
+++ scripts/verification_services.sh
@@ -0,0 +1,3 @@
+sudo systemctl status wazuh-manager
+sudo systemctl status wazuh-indexer
+sudo systemctl status wazuh-dashboard
